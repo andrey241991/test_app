d6348aca1c8f926ab8d6679c3a288007812fabf8
Andrey Kushch <droni2442@gmail.com>
2019-12-11 15:02:33 +0200

    basic side bar was added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div className="app">
-    <Header />
-  </div>
+      <Header />
+      <div className="app-wrapper">
+        <SideBar/>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/SideBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const SideBar = () => {
+    return <aside className='sidebar'>
+
+    </aside>
+}
+
+export default SideBar;
