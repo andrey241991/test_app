0fe8cb5ef1a36375eda3cf2e8f65a135ce0792dd
Andrey Kushch <droni2442@gmail.com>
2019-12-12 17:03:24 +0200

    firebase was added

--- src/components/OfficeList/index.js
+++ src/components/OfficeList/index.js
@@ -1,31 +1,79 @@
 import React, { Component } from 'react';
 import './style.css';
 import OfficeItem from '../OfficeItem';
+import * as firebase from 'firebase';
 
-const offices = [
+const firebaseConfig = {
+    apiKey: "AIzaSyCoCOFTkFvJCtj8-_F31_WXop9SBYiW9lE",
+    authDomain: "officeapp-a0ad9.firebaseapp.com",
+    databaseURL: "https://officeapp-a0ad9.firebaseio.com",
+    projectId: "officeapp-a0ad9",
+    storageBucket: "officeapp-a0ad9.appspot.com",
+    messagingSenderId: "103183867653",
+    appId: "1:103183867653:web:bc4ee164e988e70957216c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-];
 
 class OfficeList extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            offices: []
         };
     }
 
+    componentDidMount() {
+        this.getOffices();
+    }
+
+    setToDataBase() {
+        console.log('firebetToDataBasease IS CALL');
+        const db = firebase.firestore();
+        db.settings({
+            timestampsInSnapshots: true
+        });
+        const userRef = db.collection('offices').add({
+            adress: ['myAdress'],
+            email: 'myEmail',
+            phone: '+380947878'
+        });
+    }
+
+    getOffices() {
+        let items = [];
+        const db = firebase.firestore();
+        db.collection('offices').get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                let item = doc.data();
+                item = JSON.stringify(item);
+                items.push(item)
+            });
+        });
+        this.setState({
+            offices: items
+        })
+    }
+
+
     render() {
+        let { offices } = this.state;
         return (
             <div>
-                {offices.map(item => {
+                {console.log('offices in render = ', offices)}
+                {offices[0] ? <h2>{offices[0].email}</h2> : false}
+                {console.log('offices in render2  = ', offices[0].email)}
+                {/* {offices.map(item => {
+                    debugger;
                     return <OfficeItem
-                            adress={item.adress}
-                            phone={item.phone}
-                            email={item.email}
-                            fax={item.fax}
-                        />
-                })}
+                        adress={item.adress}
+                        phone={item.phone}
+                        email={item.email}
+                        fax={item.fax}
+                    />
+                })} */}
             </div>
         );
     }
