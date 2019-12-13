0f4b1573ec55034f6cd4ddbc4293cb6c9c0282f7
Andrey Kushch <droni2442@gmail.com>
2019-12-13 19:49:29 +0200

    delete and read from DB were added

--- src/components/OfficeListContainer/index.js
+++ src/components/OfficeListContainer/index.js
@@ -4,7 +4,7 @@ import OfficeList from '../OfficeList';
 import * as firebase from 'firebase';
 import { connect } from 'react-redux'
 import { setOfficesActionCreator } from '../../redux/office-reducer';
-import {generateID} from '../../utils/utils'
+import { generateID } from '../../utils/utils'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCoCOFTkFvJCtj8-_F31_WXop9SBYiW9lE",
@@ -17,8 +17,10 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 class OfficeListContainer extends React.Component {
+
 
     componentDidMount() {
         this.getOffices();
@@ -26,7 +28,6 @@ class OfficeListContainer extends React.Component {
 
     async getOffices() {
         let items = [];
-        const db = firebase.firestore();
         await db.collection('offices').get().then((snapshot) => {
             snapshot.docs.forEach(doc => {
                 let item = doc.data();
@@ -36,30 +37,37 @@ class OfficeListContainer extends React.Component {
         this.props.setOffices(items);
     }
 
-    removeOffice(id){
-        console.log('heelos id = ', id);
-        // let items = [];
-        // const db = firebase.firestore();
-        // await db.collection('offices').r  get().then((snapshot) => {
-        //     snapshot.docs.forEach(doc => {
-        //         let item = doc.data();
-        //         items.push(item)
-        //     });
-        // });
-        // this.props.setOffices(items);
+    async removeOffice(id) {
+        const officeQuery = db.collection('offices').where('id', '==', id);
+        officeQuery.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                doc.ref.delete();
+            });
+        });
+
+        let items = [];
+        await db.collection('offices').get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                let item = doc.data();
+                if (item.id !== id) {
+                    items.push(item)
+                }
+            });
+        });
+        this.props.setOffices(items);
     }
 
-    editOffice(id){
+    editOffice(id) {
         console.log('update id = ', id);
     }
 
-
-
     render() {
         return (
-            <OfficeList offices={this.props.offices}
-            removeOffice={this.removeOffice}
-            editOffice={this.editOffice} />
+            <OfficeList
+                offices={this.props.offices}
+                removeOffice={this.removeOffice.bind(this)}
+                editOffice={this.editOffice.bind(this)}
+            />
         )
     }
 }
@@ -75,7 +83,7 @@ const mapDispatchToProps = (dispatch) => {
         setOffices: (offices) => {
             dispatch(setOfficesActionCreator(offices));
         },
-        removeOffice: () =>{
+        removeOffice: () => {
             dispatch();
         },
         // sendMessage: () =>{
@@ -86,42 +94,3 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(OfficeListContainer);
-
-// OfficeListContainer = connect(mapStateToProps, mapDispatchToProps)(OfficeList);
-// export default OfficeListContainer;
-
-
-
-
-// setToDataBase() {
-//     console.log('firebetToDataBasease IS CALL');
-//     const db = firebase.firestore();
-//     db.settings({
-//         timestampsInSnapshots: true
-//     });
-//     const userRef = db.collection('offices').add({
-//         adress: ['myAdress'],
-//         email: 'myEmail',
-//         phone: '+380947878'
-//     });
-// }
-
-// getOffices() {
-//     let items = [];
-//     const db = firebase.firestore();
-//     db.collection('offices').get().then((snapshot) => {
-//         snapshot.docs.forEach(doc => {
-//             let item = doc.data();
-//             item = JSON.stringify(item);
-//             items.push(item)
-//         });
-//     });
-//     this.setState({
-//         offices: items
-//     })
-// }
-
-// componentDidMount() {
-//     this.getOffices();
-// }
-
